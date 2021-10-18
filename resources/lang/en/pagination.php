4edb7a515ea6d578652dd6840b2f7546fd75f4e7
NichePro21 <92494285+NichePro21@users.noreply.github.com>
2021-10-19 00:41:50 +0700

    Update pagination.php

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -12,7 +12,7 @@ return [
     | you want to customize your views to better match your application.
     |
     */
-
+    //edit
     'previous' => '&laquo; Previous',
     'next' => 'Next &raquo;',
 
